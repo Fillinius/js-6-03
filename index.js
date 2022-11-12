e66bf08cb44e49bbcd05284a9dbe1982082283b9
Fillinius <113914362+Fillinius@users.noreply.github.com>
2022-11-12 20:12:02 +0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-if (window.confirm('JavaScript появился в 1995 году?')) {
-  window === true, alert('Верно')
-} else {
-  window === false,
-    alert(' Утверждение было верным, JavaScript появился в 1995 году')
-}
