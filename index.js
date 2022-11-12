2216dd98c639a05f09cfb30995cb4e195b13c619
Fillinius <113914362+Fillinius@users.noreply.github.com>
2022-11-12 20:12:22 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+if (window.confirm('JavaScript появился в 1995 году?')) {
+  window === true, alert('Верно')
+} else {
+  window === false,
+    alert(' Утверждение было верным, JavaScript появился в 1995 году')
+}
+
+if (window.confirm('Спецификация JavaScript называется ECMAScript?')) {
+  window === true, alert('Верно')
+} else {
+  window === false,
+    alert(
+      ' Утверждение было верным, Спецификация JavaScript называется ECMAScript'
+    )
+}
+
+if (window.confirm('JavaScript был создан за 1 месяц?')) {
+  window === true, alert('Не верно, JavaScript был создан за 10 дней')
+} else {
+  window === false, alert(' Верно')
+}
