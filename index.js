687bbfffdfe871a8e11c8ee587022aa0b8d3d462
Fillinius <113914362+Fillinius@users.noreply.github.com>
2022-11-12 20:07:38 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+if (window.confirm('JavaScript появился в 1995 году?')) {
+  window === true, alert('Верно')
+} else {
+  window === false,
+    alert(' Утверждение было верным, JavaScript появился в 1995 году')
+}
